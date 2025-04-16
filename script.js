cc0d8f78737d19e99ad79ffc226a4c701b11eb33
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-04-17 00:29:48 +0200

    CRUD implemented

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 const API_URL = 'https://u05-music-library-api.onrender.com/api/songs';
 
+let editId = null;
+
 document.getElementById('addSongBtn').addEventListener('click', async() => {
     const title = document.getElementById('title').value;
     const artist = document.getElementById('artist').value;
     const genre = document.getElementById('genre').value;
     const rating = document.getElementById('rating').value;
 
-    const response = await fetch(API_URL, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title, artist, genre, rating }),
-    });
+    if (editId) {
+        //UPDATE ONE
+        const response = await fetch(`${API_URL}/${editId}`, {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json' },
+            body: JSON.stringify({ title, artist, genre, rating }),
+        });
 
-    const data = await response.json();
-    alert('Låt tillagd!');
+        const data = await response.json();
+        alert('Låt uppdaterad!');
+        editId = null;
+        document.getElementById('addSongBtn').textContent = 'Lägg till låt';
+    } else {
+        // POST ONE
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title, artist, genre, rating }),
+        });
+        const data = await response.json();
+        alert('Låt tillagd!');
+    }
+    document.getElementById('title').value = '';
+    document.getElementById('artist').value = '';
+    document.getElementById('genre').value = '';
+    document.getElementById('rating').value = '';
     loadSongs();
 });
 
@@ -34,3 +54,21 @@ async function loadSongs() {
         container.appendChild(div);
     });
 }
+
+async function deleteSong(id) {
+    // DELETE ONE
+    await fetch(`${API_URL}/${id}`, { method: 'DELETE' });
+    alert('Låt raderad!');
+    loadSongs();
+}
+
+function editSong(id, title, artist, genre, rating) {
+    document.getElementById('title').value = title;
+    document.getElementById('artist').value = artist;
+    document.getElementById('genre').value = genre;
+    document.getElementById('rating').value = rating;
+    editId = id;
+    document.getElementById('addSongBtn').textContent = 'Uppdatera låt';
+}
+  
+window.onload = loadSongs;
